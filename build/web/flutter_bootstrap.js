efd86af6bbcb206c9dfdad9674f78f4d11739783
Nayla311 <24111814144@mhs.unesa.ac.id>
2025-10-08 15:23:29 +0700

    rebuild web with correct base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1648654945"
+    serviceWorkerVersion: "1788978371"
   }
 });
